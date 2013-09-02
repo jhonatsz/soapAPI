aad58729ec611515f0ff94ca29689a6a4091bae4
Kaii <lcaii.sann@gmail.com>
2013-09-02 16:45:41 +0800

    update soapui

--- soapClient.php
+++ soapClient.php
@@ -1,4 +1,10 @@
 <?php
-	/*
-	 */
+try{
+  $sClient = new SoapClient('http://localhost/webservice/data.wsdl');
+  $params = "Aqila";
+  $response = $sClient->doHello($params);
+  var_dump($response);
+} catch(SoapFault $e){
+  var_dump($e);
+}
 ?>
---
+++ soapService.php
@@ -0,0 +1,13 @@
+<?php
+if(!extension_loaded("soap")){
+  dl("php_soap.dll");
+}
+ini_set("soap.wsdl_cache_enabled","0");
+$server = new SoapServer("hello.wsdl");
+function doHello($yourName){
+  return "Hello, ".$yourName;
+}
+$server->AddFunction("doHello");
+$server->handle();
+
+?>
